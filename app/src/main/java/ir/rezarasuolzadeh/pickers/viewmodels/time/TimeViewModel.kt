fe107b3c1d7e2d3893eb510fee5ec855fd988716
Reza Rasuolzadeh <reza.rassoulzadeh@gmail.com>
2024-09-02 18:22:25 +0330

    optimize time view model.

--- app/src/main/java/ir/rezarasuolzadeh/pickers/viewmodels/time/TimeViewModel.kt
+++ app/src/main/java/ir/rezarasuolzadeh/pickers/viewmodels/time/TimeViewModel.kt
@@ -10,9 +10,9 @@ class TimeViewModel : ViewModel() {
     //                                       defaults                                             //
     ////////////////////////////////////////////////////////////////////////////////////////////////
 
-    private var defaultHours24 = (0..23).map { if (it < 10) "0$it" else "$it" }
-
     private var defaultHours12 = (0..12).map { if (it < 10) "0$it" else "$it" }
+
+    private var defaultHours24 = (0..23).map { if (it < 10) "0$it" else "$it" }
 
     private var defaultMinutes = (0..59).map { if (it < 10) "0$it" else "$it" }
 
@@ -47,33 +47,68 @@ class TimeViewModel : ViewModel() {
     fun onEvent(event: TimeEvent) {
         when (event) {
             is TimeEvent.SetTimeFormat -> {
-                is12Hour.value = event.is12Hour
+                setTimeFormat(is12Hour = event.is12Hour)
             }
             is TimeEvent.SetTimeType -> {
-                timeType.value = event.timeType
+                setTimeType(timeType = event.timeType)
             }
             is TimeEvent.SetInitialTime -> {
-                event.initialHour?.let { initialHour ->
-                    hours.value = (initialHour..if (is12Hour.value) 12 else 23).map { if (it < 10) "0$it" else "$it" } + (0..<initialHour).map { if (it < 10) "0$it" else "$it" }
-                } ?: run {
-                    hours.value = if (is12Hour.value) defaultHours12 else defaultHours24
-                }
-                event.initialMinute?.let { initialMinute ->
-                    minutes.value = (initialMinute..59).map { if (it < 10) "0$it" else "$it" } + (0..<initialMinute).map { if (it < 10) "0$it" else "$it" }
-                } ?: run {
-                    minutes.value = defaultMinutes
-                }
-                event.initialSecond?.let { initialSecond ->
-                    seconds.value = (initialSecond..59).map { if (it < 10) "0$it" else "$it" } + (0..<initialSecond).map { if (it < 10) "0$it" else "$it" }
-                } ?: {
-                    seconds.value = defaultSeconds
-                }
-                event.initialTimeType?.let { initialTimeType ->
-                    timeType.value = initialTimeType
-                } ?: {
-                    timeType.value = TimeType.AM
-                }
+                setHour(hour = event.initialHour)
+                setMinute(minute = event.initialMinute)
+                setSecond(second = event.initialSecond)
+                setTimeType(timeType = event.initialTimeType)
             }
+        }
+    }
+
+    /**
+     * set the time format with given value.
+     */
+    private fun setTimeFormat(is12Hour: Boolean) {
+        this.is12Hour.value = is12Hour
+    }
+
+    /**
+     * set the time type with given value.
+     */
+    private fun setTimeType(timeType: TimeType?) {
+        timeType?.let { initialTimeType ->
+            this.timeType.value = initialTimeType
+        } ?: {
+            this.timeType.value = TimeType.AM
+        }
+    }
+
+    /**
+     * prepare hours list according to given hour value.
+     */
+    private fun setHour(hour: Int?) {
+        hour?.let { initialHour ->
+            hours.value = (initialHour..if (is12Hour.value) 12 else 23).map { if (it < 10) "0$it" else "$it" } + (0..<initialHour).map { if (it < 10) "0$it" else "$it" }
+        } ?: run {
+            hours.value = if (is12Hour.value) defaultHours12 else defaultHours24
+        }
+    }
+
+    /**
+     * prepare minutes list according to given minute value.
+     */
+    private fun setMinute(minute: Int?) {
+        minute?.let { initialMinute ->
+            minutes.value = (initialMinute..59).map { if (it < 10) "0$it" else "$it" } + (0..<initialMinute).map { if (it < 10) "0$it" else "$it" }
+        } ?: run {
+            minutes.value = defaultMinutes
+        }
+    }
+
+    /**
+     * prepare seconds list according to given second value.
+     */
+    private fun setSecond(second: Int?) {
+        second?.let { initialSecond ->
+            seconds.value = (initialSecond..59).map { if (it < 10) "0$it" else "$it" } + (0..<initialSecond).map { if (it < 10) "0$it" else "$it" }
+        } ?: {
+            seconds.value = defaultSeconds
         }
     }
 
