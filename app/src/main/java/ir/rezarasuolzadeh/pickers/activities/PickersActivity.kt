3cc6a46ea940a6751690d4bde34c7523d784515a
reza.rasoulzadeh <reza.rasoulzadeh@eniac-tech.local>
2024-10-20 15:15:44 +0330

    refactor the pickers activity code.

--- app/src/main/java/ir/rezarasuolzadeh/pickers/activities/PickersActivity.kt
+++ app/src/main/java/ir/rezarasuolzadeh/pickers/activities/PickersActivity.kt
@@ -5,13 +5,20 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import ir.rezarasuolzadeh.pickers.ui.dialog.DateDialog
+import ir.rezarasuolzadeh.pickers.ui.dialog.TimeDialog
 import ir.rezarasuolzadeh.pickers.ui.theme.PickersTheme
 import ir.rezarasuolzadeh.pickers.ui.theme.White
 import ir.rezarasuolzadeh.pickers.utils.enums.MonthType
+import ir.rezarasuolzadeh.pickers.utils.enums.TimeOutputType
+import ir.rezarasuolzadeh.pickers.utils.enums.TimeType
 
 class PickersActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -20,22 +27,54 @@ class PickersActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = White
                 ) {
-                    DateDialog(
-                        initialYear = 1401,
-                        initialMonth = MonthType.ABAN,
-                        initialDay = 23,
-                        yearRange = 1375..1403
-                    )
-//                    TimeDialog(
-//                        initialHour = 2,
-//                        initialMinute = 13,
-//                        initialSecond = 7,
-//                        initialTimeType = TimeType.PM,
-//                        is12Hour = true,
-//                        showSeconds = true
-//                    )
+                    val isTimeDialogVisible = remember { mutableStateOf(false) }
+                    val isDateDialogVisible = remember { mutableStateOf(true) }
+
+                    if (isTimeDialogVisible.value) {
+                        ShowTimeDialog {
+                            isTimeDialogVisible.value = false
+                        }
+                    }
+
+                    if (isDateDialogVisible.value) {
+                        ShowDateDialog {
+                            isDateDialogVisible.value = false
+                        }
+                    }
                 }
             }
         }
     }
+
+    /**
+     * show the time picker dialog with it's parameters.
+     */
+    @Composable
+    private fun ShowTimeDialog(onCancel: () -> Unit) {
+        TimeDialog(
+            initialHour = 2,
+            initialMinute = 13,
+            initialSecond = 7,
+            initialTimeType = TimeType.PM,
+            outputType = TimeOutputType.ENGLISH,
+            is12Hour = true,
+            showSeconds = true,
+            onCancel = onCancel
+        )
+    }
+
+    /**
+     * show the date picker dialog with it's parameters.
+     */
+    @Composable
+    private fun ShowDateDialog(onCancel: () -> Unit) {
+        DateDialog(
+            initialYear = 1401,
+            initialMonth = MonthType.ABAN,
+            initialDay = 23,
+            yearRange = 1375..1403,
+            onCancel = onCancel
+        )
+    }
+
 }
