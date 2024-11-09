24e1abf1291adbcdf250fed81c07806cc5b61fcb
reza.rasoulzadeh <reza.rasoulzadeh@eniac-tech.local>
2024-11-09 15:30:19 +0330

    update publish version.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,7 +57,7 @@ publishing {
         create<MavenPublication>("release") {
             groupId = "com.github.rezarasuolzadeh"
             artifactId = "pickers"
-            version = "1.1.0"
+            version = "1.2.0"
             afterEvaluate {
                 from(components["release"])
             }
